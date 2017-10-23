073484490bd067a4d7909357d8aa447cc0e01d27
邰广银 <273650669@qq.com>
2017-10-23 19:37:27 +0800

    base commit

---
+++ Server/routes/index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var ioSvc = require('../io/ioHelper').ioSvc;
+var msgType = require('../io/messageTpye').messageType;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/help', function(req, res, next) {
+    res.render('help');
+});
+
+router.get('/sendMsg', function(req, res, next) {
+    var type = req.query.type || msgType.public;
+    var content = req.query.content || 'none';
+    var uid = req.query.uid;
+
+    switch (type){
+        case msgType.public:
+            ioSvc.serverBroadcastMsg(content);
+            break;
+        case msgType.private:
+            if(!uid){
+                return res.send({code:400,msg:'uid参数必传'});
+            }
+            ioSvc.serverToPrivateMsg(uid,content);
+            break;
+    }
+    return res.send({code:200,msg:'发送成功'});
+});
+
+router.post('/sendMsg', function(req, res, next) {
+    var type = req.query.type || msgType.public;
+    var content = req.query.content || 'none';
+    var uid = req.query.uid;
+
+    switch (type){
+        case msgType.public:
+            ioSvc.serverBroadcastMsg(content);
+            break;
+        case msgType.private:
+            if(!uid){
+                return res.send({code:400,msg:'uid参数必传'});
+            }
+            ioSvc.serverToPrivateMsg(uid,content);
+            break;
+    }
+    return res.send({code:200,msg:'发送成功'});
+});
+
+module.exports = router;
